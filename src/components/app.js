70c19740d2a0cdff3bf2854241a0b2f6043a45cc
Adriana Jimenez <ajimenez0024@gmail.com>
2019-05-25 13:25:36 -0400

    react-video

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,27 @@
 import React, { Component } from "react";
+import ReactPlayer from "react-player";
 
 export default class App extends React.Component {
   render() {
-    return <div>Alex</div>;
+    const rootStyles = {
+      display: "flex",
+      flexDirection: "column",
+      minHeight: "100vh"
+    };
+    const bodyStyle = {
+      flex: "1"
+    };
+    return (
+      <div style={rootStyles}>
+        <div className="header">This is the title</div>
+        <div className="body" style={bodyStyle}>
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+            // playing
+          />
+        </div>
+        <div className="footer">Footer</div>
+      </div>
+    );
   }
 }
