c271bc842ef8c38de8e042e987649696191b4382
Adriana Jimenez <ajimenez0024@gmail.com>
2019-05-28 14:37:49 -0400

    added media source to video

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactPlayer from "react-player";
-import "./mediaSource.js";
+import { MEDIAURL } from "./mediaSource.js";
 
 export default class App extends React.Component {
   render() {
@@ -12,27 +12,23 @@ export default class App extends React.Component {
     const bodyStyle = {
       flex: "1"
     };
+
     return (
       <div style={rootStyles}>
         <div className="header">This is the title</div>
         <div className="body" style={bodyStyle}>
-          <div className="video">
+          <div>
             <video
               autoPlay
               controls
-              src="https://d1p7vmfdxc45kc.cloudfront.net/Sample+Videos+2.mp4"
+              className="video"
+              src={MEDIAURL}
               width={700}
               height={500}
-            >
-              No Videos{" "}
-            </video>
+            />
           </div>
-          {/* <ReactPlayer className="video"
-            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-            // playing
-          /> */}
+          <div className="footer">Footer</div>
         </div>
-        <div className="footer">Footer</div>
       </div>
     );
   }
--- src/components/mediaSource.js
+++ src/components/mediaSource.js
@@ -1,20 +1,21 @@
+export const MEDIAURL =
+  "https://d1p7vmfdxc45kc.cloudfront.net/Sample+Videos+2.mp4";
 var vidElement = document.querySelector("video");
 
-if (window.MediaSource) {
-  var mediaSource = new MediaSource();
-  vidElement.src = URL.createObjectURL(mediaSource);
-  mediaSource.addEventListener("sourceopen", sourceOpen);
-} else {
-  console.log("The Media Source Extensions API is not supported.");
-}
+// if (window.MediaSource) {
+//   var mediaSource = new MediaSource();
+//   vidElement.src = URL.createObjectURL(mediaSource);
+//   mediaSource.addEventListener("sourceopen", sourceOpen);
+// } else {
+//   console.log("The Media Source Extensions API is not supported.");
+// }
 
 function sourceOpen(e) {
   URL.revokeObjectURL(vidElement.src);
   var mime = 'video/mp4; codecs="opus, vp09.00.10.08"';
   var mediaSource = e.target;
   var sourceBuffer = mediaSource.addSourceBuffer(mime);
-  var videoUrl = "https://d1p7vmfdxc45kc.cloudfront.net/Sample+Videos+2.mp4";
-  fetch(videoUrl)
+  fetch(MEDIAURL)
     .then(function(response) {
       return response.arrayBuffer();
     })
