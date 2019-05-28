d7ce6f6ee66e4a9e2e2497d7058095b52f5542bd
Adriana Jimenez <ajimenez0024@gmail.com>
2019-05-28 13:44:06 -0400

    react video

--- src/components/app.js
+++ src/components/app.js
@@ -15,10 +15,17 @@ export default class App extends React.Component {
       <div style={rootStyles}>
         <div className="header">This is the title</div>
         <div className="body" style={bodyStyle}>
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-            // playing
-          />
+          <div className="video">
+            <video
+              autoPlay
+              controls
+              src="https://d1p7vmfdxc45kc.cloudfront.net/Sample+Videos+2.mp4"
+              width={700}
+              height={500}
+            >
+              No Videos{" "}
+            </video>
+          </div>
         </div>
         <div className="footer">Footer</div>
       </div>
