c53c7fd2674803af5bb2610f414c49dd80b327ca
Michael Cain <michaelcaincb@gmail.com>
2019-05-28 12:58:00 -0700

    Revert "Merge branch 'master' of https://github.com/adrianaj24/home-assistant"
    This reverts commit 64bb183804e496f20aec2aa369f1aee1b93b2e38, reversing
    changes made to 1eabf0169b68f8466487781ad69f2a99d3b45d20.

--- rtspVideoRecorder/src/components/app.js
+++ rtspVideoRecorder/src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactPlayer from "react-player";
-import { MEDIAURL } from "./mediaSource.js";
+import "./mediaSource.js";
 
 export default class App extends React.Component {
   render() {
@@ -12,23 +12,27 @@ export default class App extends React.Component {
     const bodyStyle = {
       flex: "1"
     };
-
     return (
       <div style={rootStyles}>
         <div className="header">This is the title</div>
         <div className="body" style={bodyStyle}>
-          <div>
+          <div className="video">
             <video
               autoPlay
               controls
-              className="video"
-              src={MEDIAURL}
+              src="https://d1p7vmfdxc45kc.cloudfront.net/Sample+Videos+2.mp4"
               width={700}
               height={500}
-            />
+            >
+              No Videos{" "}
+            </video>
           </div>
-          <div className="footer">Footer</div>
+          {/* <ReactPlayer className="video"
+            url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+            // playing
+          /> */}
         </div>
+        <div className="footer">Footer</div>
       </div>
     );
   }
