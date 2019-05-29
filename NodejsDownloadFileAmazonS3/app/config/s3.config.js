0435bf4ab0c9d5c6ae3073cf02aec70b8bf49644
Adriana Jimenez <ajimenez0024@gmail.com>
2019-05-29 17:51:01 -0400

    changes to env file

--- NodejsDownloadFileAmazonS3/app/config/s3.config.js
+++ NodejsDownloadFileAmazonS3/app/config/s3.config.js
@@ -1,18 +1,19 @@
-const AWS = require('aws-sdk');
- 
+const AWS = require("aws-sdk");
+const env = require("./.env");
+
 const s3Client = new AWS.S3({
-  accessKeyId: "AKIAVLZCPLUKXAUEHIX3",
-  secretAccessKey: "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",
-	region : "us-east-1"
+  accessKeyId: env.AWS_ACCESS_KEY,
+  secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
+  region: env.REGION
 });
- 
+
 const downloadParams = {
-  Bucket: 'michaelcain-livestream',
-  Key: ' '
+  Bucket: env.Bucket,
+  Key: " "
 };
 
 const s3 = {};
 s3.s3Client = s3Client;
 s3.downloadParams = downloadParams;
- 
+
 module.exports = s3;
