d62f3de5c9208116424f488cf9d6431bd1abb500
alex <you@example.com>
2019-06-01 19:59:30 +0000

    list objects in S3 for saved videos with AWS CLI

--- NodejsDownloadFileAmazonS3/app/controllers/s3.controller.js
+++ NodejsDownloadFileAmazonS3/app/controllers/s3.controller.js
@@ -1,5 +1,17 @@
 var stream = require("stream");
 const s3 = require("../config/s3.config.js");
+var awsCli = require('aws-cli-js');
+var Options = awsCli.Options;
+var Aws = awsCli.Aws;
+const env = require("../config/.env");
+
+var options = new Options(
+  /* accessKey    */ env.AWS_ACCESS_KEY,
+  /* secretKey    */ env.AWS_SECRET_ACCESS_KEY,
+  /* currentWorkingDirectory */ null
+);
+
+var aws = new Aws(options)
 
 async function mainFunction() {
   var key = await getLatestKeyFromS3Bucket("michaelcain-livestream");
@@ -28,16 +40,16 @@ async function getLatestKeyFromS3Bucket(bucket) {
 async function getSavedVideo(video, bucket) {
   const s3Client = s3.s3Client;
   let params = { Bucket: bucket };
-  try {
-    const response = await s3Client.listObjects(params).promise();
-    let latest = response.Contents[0];
-    for (let key of response.Contents) {
-      if (key.LastModified === video) {
-        latest = key;
-        console.log("LATEST", latest);
-      }
-    }
-    return latest;
+  console.log(params.Bucket)
+  console.log(video)
+  try { 
+    // `s3 list-objects s3://${params.Bucket} . --exclude "*" --include "${video.firstParam}*"`
+    // aws s3api list-objects --bucket myBucketName 
+    aws.command(`s3api list-objects --bucket ${params.Bucket} --prefix "${video.firstParam}"`).then(function (data) {
+      // let savedVideo = data.raw["Contents"]
+     let savedVideo = JSON.parse(data.raw)
+     console.log(savedVideo)
+    })
   } catch (error) {
     throw error;
   }
